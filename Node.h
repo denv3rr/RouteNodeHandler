5e084a86c1f520d09bd9bae63c45d271a99f58e9
denv3rr <denver.clark@g.austincc.edu>
2024-09-21 23:04:11 -0500

    setting up Node.h header for Node.cpp and the node class for function prototyping etc

---
+++ Node.h
@@ -0,0 +1,22 @@
+/* *************************************************
+ * Purpose: Node.cpp header file for creation of Node class
+ ************************************************* */
+#ifndef NODE_H
+#define NODE_H
+
+#include <vector>
+
+class Node
+{
+public:
+    Node(int id);
+    int getId() const;
+    void addNeighbor(Node *neighbor);
+    const std::vector<Node *> &getNeighbors() const;
+
+private:
+    int id;
+    std::vector<Node *> neighbors;
+};
+
+#endif // NODE_H
