b3a623093f1120ce284c41216950ffea22b10772
denv3rr <denver.clark@g.austincc.edu>
2024-09-21 23:51:33 -0500

    NPC.h file created with prototypes of NPC class functions such as get ID, get the current node, and move to another node

---
+++ NPC.h
@@ -0,0 +1,19 @@
+#ifndef NPC_H
+#define NPC_H
+
+#include "Node.h"
+
+class NPC
+{
+public:
+    NPC(int id, Node *startNode);
+    int getId() const;
+    Node *getCurrentNode() const;
+    void moveToNode(Node *node);
+
+private:
+    int id;
+    Node *currentNode;
+};
+
+#endif // NPC_H
