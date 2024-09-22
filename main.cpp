0296ed4325e84e441993a16f97d8302b6041d662
denv3rr <denver.clark@g.austincc.edu>
2024-09-22 00:48:58 -0500

    creating sample with 3 nodes initialized in circular sequence, calling the next node after it

--- main.cpp
+++ main.cpp
@@ -10,3 +10,26 @@
 
 // Local
 #include "main.h"
+
+void initializeNodes(Node &node1, Node &node2, Node &node3)
+{
+    /* Creates circular connection between the current nodes
+    by adding the next node in the list to the node making
+    the call to add the neighbor. */
+
+    node1.addNeighbor(&node2);
+    node2.addNeighbor(&node3);
+    node3.addNeighbor(&node1);
+}
+
+void initializeNPCs(TrafficManager &trafficManager, Node &node1, Node &node2)
+{
+}
+
+void simulateTraffic(TrafficManager &trafficManager, NPC &npc1, NPC &npc2)
+{
+}
+
+int main()
+{
+}
