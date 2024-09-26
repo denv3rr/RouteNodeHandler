11ba437b2143bc034628b00e1951bf834ba7e105
denv3rr <denver.clark@g.austincc.edu>
2024-09-26 00:15:21 -0500

    changed circular node creation function to proximity based and will end up running the test simulations within probably a 10km x 10km space with a field of nodes that the created objects can travel through depending on their path

--- src/Node.cpp
+++ src/Node.cpp
@@ -7,38 +7,34 @@
 
 #include "../include/Node.h"
 #include <iostream>
+#include <cmath> // for distance calculations
 
 // Node class initializer
-float x, y, z;
 Node::Node(int id, float x, float y, float z) : id(id), x(x), y(y), z(z) {}
 
 std::vector<Node> nodes;
 
 void createNode(int id)
 {
-    float x = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / 10.0));
-    float y = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / 10.0));
-    float z = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / 10.0));
+    float x = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / 10000.0)); // 10x10km area
+    float y = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / 10000.0));
+    float z = static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / 100.0)); // z for terrain elevation if needed
     nodes.push_back(Node(id, x, y, z));
-}
-
-void moveRandomly()
-{
-    x += (rand() % 3 - 1);
-    y += (rand() % 3 - 1);
-    z += (rand() % 3 - 1);
-}
-
-int Node::getId() const
-{
-    return id;
 }
 
 void Node::addNeighbor(Node *neighbor)
 {
-    neighbors.push_back(neighbor);
+    float distance = std::sqrt(std::pow((neighbor->getX() - this->getX()), 2) +
+                               std::pow((neighbor->getY() - this->getY()), 2) +
+                               std::pow((neighbor->getZ() - this->getZ()), 2));
+
+    if (distance < 1000.0) // Add neighbors within a threshold distance
+    {
+        neighbors.push_back(neighbor);
+    }
 }
 
+// Accessor functions
 const std::vector<Node *> &Node::getNeighbors() const
 {
     return neighbors;
@@ -61,11 +57,17 @@ float Node::getZ() const
 
 void initializeNodes(std::vector<Node> &nodes)
 {
-    /* Creates circular connection between the current nodes
+    /* Creates proximity connection between the current nodes
        by adding the next node in the list. */
     std::cout << "Initializing nodes..." << std::endl;
     for (size_t i = 0; i < nodes.size(); ++i)
     {
-        nodes[i].addNeighbor(&nodes[(i + 1) % nodes.size()]);
+        for (size_t j = 0; j < nodes.size(); ++j)
+        {
+            if (i != j)
+            {
+                nodes[i].addNeighbor(&nodes[j]); // Add neighbors based on distance
+            }
+        }
     }
 }
