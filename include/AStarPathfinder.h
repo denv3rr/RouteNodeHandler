25d8521ac0723664b715d83ff284380048d4c950
denv3rr <denver.clark@g.austincc.edu>
2024-09-30 12:22:19 -0500

    updating A* class to be handled under pathfinder handler - next to add is Dijkstra and probably BFS

--- include/AStarPathfinder.h
+++ include/AStarPathfinder.h
@@ -1,20 +1,18 @@
-#ifndef ASTARPATHFINDER_H
-#define ASTARPATHFINDER_H
-
-#include <vector>
-#include <unordered_map>
-#include <memory>
+#pragma once
+#include "PathfindingManager.h"
 #include "Node.h"
+#include <unordered_map>
+#include <vector>
+#include <memory>
 
-class AStarPathfinder
+class AStarPathfinder : public PathfindingManager
 {
 public:
-    std::vector<Node *> findPath(std::shared_ptr<Node> startNode, std::shared_ptr<Node> goalNode);
-    std::vector<Node *> reconstructPath(std::unordered_map<std::shared_ptr<Node>, std::shared_ptr<Node>> &cameFrom, std::shared_ptr<Node> current);
+    // Find path implementation for A* algorithm
+    std::vector<Node *> findPath(std::shared_ptr<Node> startNode, std::shared_ptr<Node> goalNode) override;
 
 private:
     float heuristicCostEstimate(const std::shared_ptr<Node> &start, const std::shared_ptr<Node> &goal) const;
     float distance(const std::shared_ptr<Node> &start, const std::shared_ptr<Node> &goal) const;
+    std::vector<Node *> reconstructPath(std::unordered_map<std::shared_ptr<Node>, std::shared_ptr<Node>> &cameFrom, std::shared_ptr<Node> current);
 };
-
-#endif
