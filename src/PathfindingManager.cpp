2db571449a261240ccb7e1786d3169811871346b
denv3rr <denver.clark@g.austincc.edu>
2024-10-05 23:57:34 -0500

    adding file for PathfindingManager.cpp that will handle dynamic switching between pathfinding algorithms for best efficiency

---
+++ src/PathfindingManager.cpp
@@ -0,0 +1,8 @@
+#include "../include/PathfindingManager.h"
+#include "../include/Node.h"
+#include <iostream>
+
+std::vector<Node *> PathfindingManger::findPath(std::share_ptr<Node> startNode, std::shared_ptr<Node> goalNode)
+{
+    //
+}
