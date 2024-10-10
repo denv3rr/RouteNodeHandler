acd6d1b23660abfb72752bc487f98bdfa75af618
denv3rr <denver.clark@g.austincc.edu>
2024-10-09 20:21:56 -0500

    creating cpp file for dijsktrapathfinder class functionality. using findPath() from IPathfindingAlgorithm.h: 'virtual std::vector<Node *> findPath(std::shared_ptr<Node> startNode, std::shared_ptr<Node> goalNode) = 0;'

---
+++ src/DijkstraPathfinder.cpp
@@ -0,0 +1,10 @@
+#include "../include/DijkstraPathfinder.h"
+#include <queue>
+#include <unordered_map>
+#include <iostream>
+#include <limits>
+
+std::vector<Node *> DijkstraPathfinder::findPath(std::shared_ptr<Node> startNode, std::shared_ptr<Node> goalNode)
+{
+    std::cout << "Using Dijkstra's algorithm...\n";
+}
