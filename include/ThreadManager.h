a94978f63687ece60613d06ae00e54ecb4481b6b
denv3rr <denver.clark@g.austincc.edu>
2024-10-17 13:55:22 -0500

    create threadmanager file set for use of multithreading during handling of entity behavior for multiple entities as opposed to sequential updates in the current state

---
+++ include/ThreadManager.h
@@ -0,0 +1,11 @@
+#ifndef THREAD_MANAGER_H
+#define THREAD_MANAGER_H
+
+#include "Node.h"
+#include "TrafficManager.h"
+#include "PathfindingManager.h"
+#include <thread>
+#include <mutex>
+#include <vector>
+
+#endif // THREAD_MANAGER_H
