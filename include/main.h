672948657b28f248c349ebed7d819653d5df77e9
denv3rr <denver.clark@g.austincc.edu>
2024-10-18 19:27:52 -0500

    progress bar for entity data

--- include/main.h
+++ include/main.h
@@ -5,14 +5,9 @@
 #include "NodeManager.h"
 #include "TrafficManager.h"
 #include "PathfindingManager.h"
-#include "NPC.h"
-#include "Vehicle.h"
+#include "InitializationManager.h"
+#include "ThreadManager.h"
 #include <vector>
 #include <unistd.h>
-
-// Function declarations for initializing NPCs and Vehicles
-void initializeNPCs(TrafficManager &trafficManager, const std::vector<std::shared_ptr<Node>> &nodes, std::vector<NPC> &npcs, int numNPCs);
-void initializeVehicles(TrafficManager &trafficManager, const std::vector<std::shared_ptr<Node>> &nodes, std::vector<Vehicle> &vehicles, int numVehicles);
-void simulateTraffic(TrafficManager &trafficManager, std::vector<NPC> &npcs, std::vector<Vehicle> &vehicles, NodeManager &nodeManager, PathfindingManager &pathfindingManager);
 
 #endif // MAIN_H
